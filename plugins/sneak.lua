faeea381869475a6f2a10b7f8ff86c516c2f6542
Johnny Urosevic <themagicalcake@gmail.com>
2025-10-17 01:11:33 -0700

    Delete old plugins folder

--- plugins/sneak.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'justinmk/vim-sneak', -- Multiline f and t
-  keys = {
-    { 'f', '<Plug>Sneak_f', desc = 'Multiline f' },
-    { 'F', '<Plug>Sneak_F', desc = 'Multiline F' },
-    { 't', '<Plug>Sneak_t', desc = 'Multiline t' },
-    { 'T', '<Plug>Sneak_T', desc = 'Multiline T' },
-  },
-}
--- plugins/theme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'cpea2506/one_monokai.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    vim.cmd.colorscheme 'one_monokai'
-
-    vim.cmd.hi 'Comment gui=none'
-  end,
-}
