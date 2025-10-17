02eca8db28e8681099579aac4297de1b7ab570e2
Johnny Urosevic <themagicalcake@gmail.com>
2025-10-17 01:10:53 -0700

    Highlight references to symbol under cursor

--- autocmd.lua
+++ autocmd.lua
@@ -5,3 +5,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+local highlight_augroup = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+  group = highlight_augroup,
+  callback = vim.lsp.buf.document_highlight,
+})
+
+vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+  group = highlight_augroup,
+  callback = vim.lsp.buf.clear_references,
+})
